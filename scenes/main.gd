b4a8b9232f77135ca7fecb1984f9f688f9a067c6
Dalvin Josias Sejour <dalvin.josias@gmail.com>
2023-09-19 22:05:11 -0700

    dry it up

--- scenes/main.gd
+++ scenes/main.gd
@@ -4,10 +4,13 @@ const MAX_HEALTH = 5
 var health = MAX_HEALTH
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	update_health_ui()
+	
+func update_health_ui():
 	set_health_label()
 	$HealthBarProgress.max_value = MAX_HEALTH
 	set_health_bar()
-
+	
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		damage()
