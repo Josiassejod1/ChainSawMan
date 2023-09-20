b36e054c3b156d941b84420e143e21649318ba7a
Dalvin Josias Sejour <dalvin.josias@gmail.com>
2023-09-19 21:59:16 -0700

    health bar

---
+++ scenes/main.gd
@@ -0,0 +1,28 @@
+extends Node
+
+const MAX_HEALTH = 5
+var health = MAX_HEALTH
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_health_label()
+	$HealthBarProgress.max_value = MAX_HEALTH
+	set_health_bar()
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		damage()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+func damage():
+	health -= 1
+	if health < 0:
+		health = MAX_HEALTH
+	set_health_bar()
+		
+func set_health_bar():
+	$HealthBarProgress.value = health
+	
+func set_health_label():
+	$HealthBarLabel.text = "Health: %s" % health
+	
